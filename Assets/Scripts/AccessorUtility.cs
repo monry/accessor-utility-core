6e3c65969954b25ccd504a65c49e1d63292bf87b
Tetsuya Mori <monry84@gmail.com>
2017-07-14 13:05:30 +0900

    :seedling: Initial commit

---
+++ Assets/Scripts/AccessorUtility.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccessorUtility {
+
+    /// <summary>
+    /// 任意のオブジェクトのインスタンスをキーにした参照ディクショナリ
+    /// </summary>
+    /// <remarks>弱参照っぽい実装を Getter 内に施している</remarks>
+    /// <typeparam name="TKey">キーにするインスタンスの型</typeparam>
+    /// <typeparam name="TValue">インスタンス格納用のディクショナリ</typeparam>
+    public class BasicRefernceMap<TKey, TValue> : Dictionary<TKey, TValue> where TValue : class, new() {
+
+        public new TValue this[TKey key] {
+            get {
+                TValue value;
+                this.TryGetValue(key, out value);
+                if (value == default(TValue)) {
+                    value = new TValue();
+                }
+                return value;
+            }
+            set {
+                base[key] = value;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Getter を拡張したインスタンス保存用のディクショナリ
+    /// </summary>
+    /// <remarks>KeyNotFoundException を防いでいる</remarks>
+    /// <typeparam name="TValue">値の型</typeparam>
+    public class BasicInstanceMap<TValue> : Dictionary<Type, TValue> {
+
+        public new TValue this[Type key] {
+            get {
+                TValue value;
+                this.TryGetValue(key, out value);
+                return value;
+            }
+            set {
+                base[key] = value;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 任意のオブジェクトに型ベースのプロパティアクセサを提供するためのクラス
+    /// </summary>
+    /// <typeparam name="TKey">キーにするオブジェクトの型</typeparam>
+    /// <typeparam name="TValue">値の型</typeparam>
+    /// <typeparam name="TReferenceMap">参照管理ディクショナリの型</typeparam>
+    /// <typeparam name="TInstanceMap">インスタンス管理ディクショナリの型</typeparam>
+    public class AccessorUtility<TKey, TValue, TReferenceMap, TInstanceMap> where TReferenceMap : BasicRefernceMap<TKey, TInstanceMap>, new() where TInstanceMap : BasicInstanceMap<TValue>, new() {
+
+        /// <summary>
+        /// インスタンスのディクショナリのディクショナリ
+        /// </summary>
+        /// <remarks>本当は ConditionalWeakTable を使いたいのだが、 .NET 4.0 以降の機能なので無理。</remarks>
+        internal static TReferenceMap instanceMapMap = new TReferenceMap();
+
+    }
+
+    /// <summary>
+    /// 任意のオブジェクトに型ベースのプロパティアクセサを提供するためのクラス
+    /// </summary>
+    /// <remarks>インスタンス管理ディクショナリを拡張しない場合はこちら</remarks>
+    /// <typeparam name="TKey">キーにするオブジェクトの型</typeparam>
+    /// <typeparam name="TValue">値の型</typeparam>
+    public class AccessorUtility<TKey, TValue> : AccessorUtility<TKey, TValue, BasicRefernceMap<TKey, BasicInstanceMap<TValue>>, BasicInstanceMap<TValue>> {
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,61 @@
+# What?
+
+アクセサ (もどき) として任意のオブジェクトからアクセスさせる際の型ベースのデータストアを提供します。
+
+# Why?
+
+* `ConditionalWeakTable` が使いたい！
+* しかし Unity は2017年7月現在、 .NET 3.5 系である
+    * Experimental として .NET 4.6 を選択可能ではある
+* 無いなら作っちまおう！
+
+# Install
+
+```shell
+$ npm install @umm/accessor-utility-core
+```
+
+# Usage
+
+* 正直、このパッケージだけでは殆ど何もできません。
+* こいつが提供する `AccessorUtility` を継承したクラスをこさえてください。
+* んで、それっぽい interface でも書いて、それの拡張クラスとしてアクセサもどきを実装します。
+
+```csharp
+using UnityEngine;
+using AccessorUtility;
+
+// 別に継承しなくても良いんだけど、毎回 Generic 書くのダルいので
+public class SampleAccessor : AccessorUtility<MonoBehaviour, Component> {}
+
+// interface として提供するコトで、ちょっと DI っぽく実装できます
+public interface IRectTransformAccessor {
+}
+
+public static class RectTransformAccessorExtension {
+
+    // この拡張メソッド内でデータストアにインスタンスを保持します
+    // 型ベースなので、複数のキーを持たせて…、みたいなのは無理っす
+    public static RectTransform RectTransform(this IRectTransformAccessor self) {
+        // ココが強引なんだよなぁ…。
+        MonoBehaviour instance = (MonoBehaviour)self;
+        return SampleAccessor.instanceMapMap[instance][typeof(RectTransform)];
+    }
+
+}
+
+public class Hoge : MonoBehaviour, IRectTransformAccessor {
+
+    public void Start() {
+        Debug.Log(this.RectTransform().sizeDelta);
+    }
+
+}
+```
+
+# License
+
+Copyright (c) 2017 Tetsuya Mori
+
+Released under the MIT license, see [LICENSE.txt](LICENSE.txt)
+
